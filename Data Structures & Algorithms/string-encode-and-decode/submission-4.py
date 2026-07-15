02c391acd8312f7f9809f784fe8bf5ef8b06b4b5
Himanshu Bhardwaj <himanshu.bartdwaj@gmail.com>
2026-07-15 16:47:24 +0530

    Add: string-encode-and-decode - submission-4

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-4.py
@@ -0,0 +1,17 @@
+class Solution:
+    
+    def encode(self, strs: List[str]) -> str:
+        if len(strs)==0: 
+            return "‡"
+        encoded_str="€".join(str for str in strs)
+        print(encoded_str)
+        return encoded_str
+
+
+    def decode(self, s: str) -> List[str]:
+        if s == "": 
+            return [""]
+        elif s == "‡": 
+            return []
+        decoded_strs=s.split("€")
+        return decoded_strs
